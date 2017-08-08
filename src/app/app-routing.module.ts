13c3a6ba6c57478f869c0072d71a2dc1a95ece26
zjhuang <zjhuang@ZJteki-MacBook-Pro.local>
2017-08-09 00:09:39 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ListsComponent } from './lists/lists.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
+    component: ListsComponent,
+    children: []
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
     children: []
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ListsComponent } from './lists/lists.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    ListsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
